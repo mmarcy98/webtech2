902015ed4531c5cf04ae944df78a41bf2ede7455
mmarcy98 <36632158+mmarcy98@users.noreply.github.com>
2021-06-23 12:02:47 +0200

    Add files via upload

---
+++ backend/src/config.ts
@@ -0,0 +1,11 @@
+const config = {
+  mongo: {
+    path: 'localhost:27017/webtech2_strebadaniel'
+  },
+  server: {
+    port: 3000,
+    secret: 'webtech2_strebadaniel'
+  }
+};
+
+export default config;
